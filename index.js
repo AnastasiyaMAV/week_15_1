cfce1fd727056bf48a482bd70df2bb5af69c5889
AnastasiyaMAV <mihalukova112@mail.ru>
2021-09-13 10:56:14 +0200

    Задание выполнено

---
+++ index.js
@@ -0,0 +1,36 @@
+function plus() {
+    let firstNumber = Number(document.getElementById('number1').value);
+    let secondNumber = Number(document.getElementById('number2').value);
+    
+    resultNumber = firstNumber + secondNumber;
+    document.getElementById('result').innerHTML = resultNumber;
+}
+
+function minus() {
+    let firstNumber = Number(document.getElementById('number1').value);
+    let secondNumber = Number(document.getElementById('number2').value);
+    
+    resultNumber = firstNumber - secondNumber;    
+    document.getElementById('result').innerHTML = resultNumber;
+}
+
+function divide() {
+    let firstNumber = Number(document.getElementById('number1').value);
+    let secondNumber = Number(document.getElementById('number2').value);
+    
+    if(secondNumber != 0){
+        resultNumber = firstNumber / secondNumber;
+    } else {
+        resultNumber = 'Деление на ноль невозможно';
+    }
+
+    document.getElementById('result').innerHTML = resultNumber;
+}
+
+function multiply() {
+    let firstNumber = Number(document.getElementById('number1').value);
+    let secondNumber = Number(document.getElementById('number2').value);
+    
+    resultNumber = firstNumber * secondNumber;    
+    document.getElementById('result').innerHTML = resultNumber;
+}
